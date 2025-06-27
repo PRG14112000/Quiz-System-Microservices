a859b20356b2b720a87bf92ef5b7c4d7784adbbc
pratikgupta142000 <1941012585.q.pratikgupta@gmail.com>
2025-06-27 23:17:45 +0530

    first commit

---
+++ QuizService/src/main/java/com/quiz/controller/QuizController.java
@@ -0,0 +1,33 @@
+package com.quiz.controllers;
+
+import com.quiz.services.QuizService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizController {
+
+    private QuizService quizService;
+    
+    public QuizController(QuizService quizService) {
+        this.quizService = quizService;
+    }
+    
+    //create 
+    @PostMapping
+    public Quiz create(@RequestBody Quiz quiz) {
+        return quizService.add(quiz);
+    }
+
+    // get all
+    @GetMapping
+    public List<Quiz> get() {
+        return quizService.get();
+    }
+
+    @GetMapping("/{id}")
+    public Quiz getOne(@PathVariable Long id) {
+        return quizService.get(id);
+    }
+}
