344fa76040b5967bebad7cd9f4f43815f4403228
pratikgupta142000 <1941012585.q.pratikgupta@gmail.com>
2025-07-05 10:54:09 +0530

    feignclient implemented

--- QuizService/src/main/java/com/quiz/entities/Quiz.java
+++ QuizService/src/main/java/com/quiz/entities/Quiz.java
@@ -9,16 +9,34 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Quiz {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    
+    transient private List<Question> questions;
+
+//    public List<Question> getQuestions() {
+//        return questions;
+//    }
+//
+//    public void setQuestions(List<Question> questions) {
+//        this.questions = questions;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
 }
--- QuizService/src/main/java/com/quiz/services/impl/QuizServiceImpl.java
+++ QuizService/src/main/java/com/quiz/services/impl/QuizServiceImpl.java
@@ -1,10 +1,13 @@
 package com.quiz.services.impl;
 
 import com.quiz.entities.Quiz;
+import com.quiz.services.QuestionClient;
 import com.quiz.services.QuizService;
 import com.quiz.repositories.QuizRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 // Make this annotation so that later we can autowire it 
@@ -13,8 +16,11 @@ public class QuizServiceImpl implements QuizService{
 
     private QuizRepository quizRepository;
 
-    public QuizServiceImpl(QuizRepository quizRepository) {
+    private QuestionClient questionClient;
+
+    public QuizServiceImpl(QuizRepository quizRepository, QuestionClient questionClient) {
         this.quizRepository = quizRepository;
+        this.questionClient = questionClient;
     }
 
     @Override
@@ -24,11 +30,22 @@ public class QuizServiceImpl implements QuizService{
 
     @Override
     public List<Quiz> get() {
-        return quizRepository.findAll();
+        //Fetch all the quizzes
+        List<Quiz> quizzes = quizRepository.findAll();
+
+        // set questions to each quizzes
+        List<Quiz> newQuizList = quizzes.stream().map(quiz -> {
+            quiz.setQuestions(questionClient.getQuestionOfQuiz(quiz.getId()));
+            return quiz;
+        }).collect(Collectors.toList());
+
+        return newQuizList;
     }
 
     @Override
     public Quiz get(Long id){
-        return quizRepository.findById(id).orElseThrow(() -> new RuntimeException("Quiz Not Found"));
+        Quiz quiz = quizRepository.findById(id).orElseThrow(() -> new RuntimeException("Quiz Not Found"));
+        quiz.setQuestions(questionClient.getQuestionOfQuiz(quiz.getId()));
+        return quiz;
     }
 }
